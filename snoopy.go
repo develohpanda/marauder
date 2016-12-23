8b3e562a56b8f074cbfa5acf7527847b5f55d826
Opender Singh <opender94@gmail.com>
2016-12-23 22:36:00 +1300

    Downloads messages

--- snoopy.go
+++ snoopy.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,20 +11,52 @@ import (
 
 func main() {
 	rp := validateArguments()
-	readFirstPage(rp)
+
+	var msgs []message
+	decodedResponse := readFirstPage(rp)
+
+	for {
+		msgs = append(msgs, decodedResponse.Data...)
+
+		fmt.Println(len(msgs))
+		if len(decodedResponse.Paging.Next) <= 0 {
+			fmt.Println("Done")
+			break
+		}
+		decodedResponse = readRemainingPages(decodedResponse.Paging.Next)
+	}
 }
 
-func readFirstPage(rp runParams) {
+func readRemainingPages(fullURL string) messagesResponse {
+	resp, err := http.Get(fullURL)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		panic(err)
+	}
+	return decodeJSONResponse(processResponse(resp))
+}
+
+func decodeJSONResponse(byt []byte) messagesResponse {
+	res := messagesResponse{}
+	err := json.Unmarshal(byt, &res)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func readFirstPage(rp runParams) messagesResponse {
 	baseURL := fmt.Sprintf("https://graph.facebook.com/v2.3/%s/messages", rp.threadID)
 	req, err := http.NewRequest("GET", baseURL, nil)
 	if err != nil {
-		log.Fatal("Could not create request")
-		return
+		panic(err)
 	}
 
 	q := req.URL.Query()
 	q.Add("access_token", rp.accessToken)
-	q.Add("limit", "2")
+	q.Add("limit", "500")
 
 	req.URL.RawQuery = q.Encode()
 	client := http.Client{}
@@ -33,21 +66,19 @@ func readFirstPage(rp runParams) {
 		defer resp.Body.Close()
 	}
 	if err != nil {
-		log.Fatal("Could not load data from graph api")
-		return
+		panic(err)
 	}
 
-	fmt.Println()
-	fmt.Println(processResponse(resp))
+	return decodeJSONResponse(processResponse(resp))
 }
 
-func processResponse(resp *http.Response) string {
+func processResponse(resp *http.Response) []byte {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	return string(body)
+	return body
 }
 
 func validateArguments() runParams {
@@ -68,4 +99,46 @@ type runParams struct {
 	accessToken string
 	threadID    string
 	keywords    []string
+}
+
+type messagesResponse struct {
+	Data   []message `json:"data"`
+	Paging paging    `json:"paging"`
+}
+
+type paging struct {
+	Previous string `json:"previous"`
+	Next     string `json:"next"`
+}
+
+type message struct {
+	ID          string `json:"id"`
+	CreatedTime string `json:"created_time"`
+	Tags        struct {
+		Data []struct {
+			Name string `json:"name"`
+		} `json:"data"`
+	} `json:"tags"`
+	From struct {
+		Name  string `json:"name"`
+		Email string `json:"email"`
+		ID    string `json:"id"`
+	} `json:"from"`
+	To struct {
+		Data []struct {
+			Name  string `json:"name"`
+			Email string `json:"email"`
+			ID    string `json:"id"`
+		} `json:"data"`
+	} `json:"to"`
+	Message string `json:"message"`
+	Shares  struct {
+		Data []struct {
+			ID          interface{} `json:"id"`
+			Link        string      `json:"link"`
+			Name        string      `json:"name"`
+			Description interface{} `json:"description"`
+			Picture     interface{} `json:"picture"`
+		} `json:"data"`
+	} `json:"shares,omitempty"`
 }
